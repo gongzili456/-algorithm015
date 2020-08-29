e10f7ef360a49bdd4c3131a0a9216161e9fad34c
liuguili <gongzili456@gmail.com>
2020-08-29 22:00:01 +0800

    week 1

---
+++ Week_01/66.加一.go
@@ -0,0 +1,26 @@
+package leetcode
+
+/*
+ * @lc app=leetcode.cn id=66 lang=golang
+ *
+ * [66] 加一
+ */
+
+// @lc code=start
+func plusOne(digits []int) []int {
+	for i := len(digits) - 1; i >= 0; i-- {
+		if digits[i] < 9 {
+			digits[i]++
+			return digits
+		}
+
+		digits[i] = 0
+	}
+
+	digits = make([]int, len(digits)+1)
+	digits[0] = 1
+
+	return digits
+}
+
+// @lc code=end
